a44daa9b81f87216da20e21259df90a584255f32
Sunil Jaiswal <sunilj@spottransittech.com>
2025-06-05 11:57:15 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab001_HelloWorld {
+    public  static  void main(String...a){
+        System.out.println("Hello Guys...I am running Java Program..");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab002_HW_EXTRA.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab002_HW_EXTRA {
+    public static void main(String...a){
+        System.out.println(2+3);
+    }
+}
